bed3ff175b43c1028fad0413901d0ece6c1b578a
muhammadsadiqhussain <sh2453621@gmail.com>
2025-03-02 17:10:29 +0500

    js-assignment-2

---
+++ assignment.js
@@ -0,0 +1,29 @@
+let userName;
+
+let myName = "sadiq";
+
+let message;
+
+message = "Hello World";
+
+alert(message);
+
+let studentName = "Sadiq";
+let age = "24";
+let grade = "B";
+let course = "Web & App Development";
+let orginaization = "SMIT";
+
+alert("Student Name: " + studentName);
+alert("age: " + age);
+alert("grade: " + grade);
+alert("course: " + course);
+alert("orginaization: " + orginaization);
+
+let message1;
+
+message1 = "Pizza\nPizz\nPiz\nPi\nP";
+
+alert(message1);
+
+
